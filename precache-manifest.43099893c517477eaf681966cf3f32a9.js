414b0d8b3fd80b364f53ea6b10286e91bd184f41
Kellyson Penha <kellyson.penha@gmail.com>
2020-02-26 00:31:23 -0300

    Test with react app

---
+++ precache-manifest.43099893c517477eaf681966cf3f32a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257f1e1c64b3652ed8387e4dd46ce2a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c4233af9e82de1e71f4",
+    "url": "/static/css/main.d2892f95.chunk.css"
+  },
+  {
+    "revision": "5532e231de55f70a3f31",
+    "url": "/static/js/2.7fdf069d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7fdf069d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4233af9e82de1e71f4",
+    "url": "/static/js/main.22f9f7f2.chunk.js"
+  },
+  {
+    "revision": "9af90a3bdb44f1b1746a",
+    "url": "/static/js/runtime-main.fb18d836.js"
+  },
+  {
+    "revision": "23586afcbed34f110de70937470877cf",
+    "url": "/static/media/LineIcons.23586afc.eot"
+  },
+  {
+    "revision": "6106e3e25c6e52ebdceb846dc3f717b0",
+    "url": "/static/media/LineIcons.6106e3e2.woff"
+  },
+  {
+    "revision": "6e9fb56e13ffdf56a053e86545cafe3e",
+    "url": "/static/media/LineIcons.6e9fb56e.ttf"
+  },
+  {
+    "revision": "a0ed6adf3563db2c6d93a0f2d4d2929e",
+    "url": "/static/media/LineIcons.a0ed6adf.svg"
+  }
+]);
